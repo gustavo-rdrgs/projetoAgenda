94c637d58fec7065505803b5031407f6630b032a
gustavo-rdrgs <gustavorsantos2713@gmail.com>
2024-08-20 00:27:40 -0300

    atualizando o código

--- src/main/java/br/ufpb/dcx/gustavo/rodrigues/agenda/GravadorDeDados.java
+++ src/main/java/br/ufpb/dcx/gustavo/rodrigues/agenda/GravadorDeDados.java
@@ -1,4 +1,24 @@
 package br.ufpb.dcx.gustavo.rodrigues.agenda;
 
+import java.io.*;
+import java.util.HashMap;
+import java.io.FileInputStream;
 public class GravadorDeDados {
-}
+
+        public static final String ARQUIVO_CONTATOS = "contatos.dat";
+
+        public HashMap<String, Contato> recuperarContatos() throws IOException{
+            try (ObjectInputStream in = new ObjectInputStream(new FileInputStream(ARQUIVO_CONTATOS))){
+                return (HashMap<String, Contato>) in.readObject();
+            } catch (ClassNotFoundException e) {
+                throw new IOException(e);
+            }
+        }
+
+        public void salvarContatos(HashMap<String,Contato> contatos) throws IOException {
+            try (ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream(ARQUIVO_CONTATOS))){
+                out.writeObject(contatos);
+            }
+        }
+
+    }
---
+++ src/test/java/br/ufpb/dcx/gustavo/rodrigues/agenda/GravadorDeDadosTest.java
@@ -0,0 +1,49 @@
+package br.ufpb.dcx.gustavo.rodrigues.agenda;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+
+public class GravadorDeDadosTest {
+
+    @Test
+    public void testaGravacao() throws IOException {
+        File arquivoContatos = new File(GravadorDeDados.ARQUIVO_CONTATOS);
+        if (arquivoContatos.exists()){
+            arquivoContatos.delete();
+        }
+        GravadorDeDados gravadorDeDados = new GravadorDeDados();
+        File arquivo = new File(GravadorDeDados.ARQUIVO_CONTATOS);
+        assertFalse(arquivo.exists());
+        HashMap<String, Contato> contatosMap = new HashMap<>();
+        contatosMap.put("Ayla", new Contato("Ayla", 5, 10));
+        gravadorDeDados.salvarContatos(contatosMap);
+        assertTrue(arquivo.exists());
+        arquivo.delete();
+    }
+
+    @Test
+    public void testaRecuperacao() throws IOException {
+        File arquivoContatos = new File(GravadorDeDados.ARQUIVO_CONTATOS);
+        if (arquivoContatos.exists()){
+            arquivoContatos.delete();
+        }
+        GravadorDeDados gravadorDeDados = new GravadorDeDados();
+        File arquivo = new File(GravadorDeDados.ARQUIVO_CONTATOS);
+        assertFalse(arquivo.exists());
+        HashMap <String, Contato> contatosMap = new HashMap<>();
+        contatosMap.put("Ayla", new Contato("Ayla",5,10));
+        gravadorDeDados.salvarContatos(contatosMap);
+        assertTrue(arquivo.exists());
+        HashMap<String, Contato> contatosRecuperados = gravadorDeDados.recuperarContatos();
+        Contato c1 = contatosRecuperados.get("Ayla");
+        assertEquals(5, c1.getDiaAniversario());
+        assertEquals(10, c1.getMesAniversario());
+        assertEquals("Ayla", c1.getNome());
+        arquivo.delete();
+    }
+
+}
