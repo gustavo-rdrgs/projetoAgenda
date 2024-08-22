1d7297669c7286377fdc1dc137fbec8299913359
gustavo-rdrgs <gustavorsantos2713@gmail.com>
2024-08-22 10:56:01 -0300

    main e jframe

---
+++ src/main/java/br/ufpb/dcx/gustavo/rodrigues/agenda/AgendaGUIV2.java
@@ -0,0 +1,8 @@
+package br.ufpb.dcx.gustavo.rodrigues.agenda;
+
+import javax.swing.*;
+
+public class AgendaGUIV2 extends JFrame {
+    JLabel linha1, linha2;
+
+}
---
+++ src/main/java/br/ufpb/dcx/gustavo/rodrigues/agenda/MainApp.java
@@ -0,0 +1,19 @@
+package br.ufpb.dcx.gustavo.rodrigues.agenda;
+
+import javax.swing.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
+
+public class MainApp {
+    public static void main(String[] args ){
+        JFrame janela = new AgendaGUI();
+        janela.setVisible(true);
+        WindowListener fechadorDeJanelaPrincipal = new WindowAdapter(){
+            public void windowClosing(WindowEvent e){
+                System.exit(0);
+            }
+        };
+        janela.addWindowListener(fechadorDeJanelaPrincipal);
+    }
+}
